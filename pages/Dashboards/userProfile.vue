49ab775911efa7a413a812873341a5dfa0fcb9b4
christianokuru <okuruchristian@gmail.com>
2025-04-12 12:57:10 +0100

    Added a few pages

---
+++ pages/Dashboards/userProfile.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>hello username</h1>
+    </div>
+</template>
---
+++ pages/signin.vue
@@ -0,0 +1,9 @@
+<script setup>
+  // Your script code here
+</script>
+
+<template>
+  <div>
+    <h1>Signin Page</h1>
+  </div>
+</template>
