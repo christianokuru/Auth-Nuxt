2f23e4a3ce6b5413079e9d37531a0e9965963293
christianokuru <okuruchristian@gmail.com>
2025-04-12 11:40:16 +0100

    Successfully fetched data from the database on atlas

---
+++ server/api/users.js
@@ -0,0 +1,10 @@
+import User from '../models/User';
+
+export default defineEventHandler(async () => {
+  try {
+    const users = await User.find(); // Fetch all documents in the User collection
+    return users; // Return the data as JSON
+  } catch (error) {
+    return { error: 'Failed to fetch users', details: error.message };
+  }
+});
