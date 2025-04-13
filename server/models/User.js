43f7fc378ca863d25e4c2889a6612f3948a82900
christianokuru <okuruchristian@gmail.com>
2025-04-13 16:04:41 +0100

    updated the user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true, // remove extra spaces
+    lowercase: true,
   },
   username: {
     type: String,
     required: true,
+    trim: true,
   },
   password: {
     type: String,
@@ -19,6 +23,33 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 })
+
+//pre-save hook to hash password before saving
+userSchema.pre('save', async function(next) {
+  // Only hash the password if it has been modified
+  if (!this.isModified('password')) return next();
+
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10)
+    // Hash the password with the salt
+    const hash = await bcrypt.hash(this.password, salt)
+    // Replace the password with the hash
+    this.password = hash
+    if (typeof next === 'function') {
+      next();
+    }
+  } catch (error) {
+    if (typeof next === 'function') {
+      next(error);
+    }
+  }
+})
+
+// Method to compare passwords during login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password)
+}
 
 // This line ensures it only defines the model if it's not already defined
 const User = mongoose.models.User || mongoose.model('User', userSchema)
