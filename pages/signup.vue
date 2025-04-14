f1e37ef78f69f6d124d38e78bc05cb4838041e07
christianokuru <okuruchristian@gmail.com>
2025-04-14 11:10:52 +0100

    working on the forms

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,21 +1,46 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const data = reactive({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-})
+  username: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+});
 </script>
 
 <template>
-    <div>
-        <h1>Signup Page</h1>
+  <div class="w-1/2 mx-auto flex flex-col mt-[110px]">
+    <div class="mb-[15px]">
+      <h1 class="text-3xl">Create an Account</h1>
     </div>
     <div>
-        <form @submit.prevent=submitForm>
+      <form @submit="submitForm">
+        <div class="grid gap-7 border">
+          <div class="grid gap-3">
+            <p>Email Address</p>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="youremail@gmail.com"
+              v-model="data.email"
+              class="border h-[50px] px-3 cursor-pointer"
+            />
+          </div>
 
-        </form>
+          <div class="grid gap-3">
+            <p>Username</p>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              v-model="data.username"
+            />
+          </div>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
